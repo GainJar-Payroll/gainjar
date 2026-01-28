2f8496aa9e842d274e5e6ed1982687563323729b
raihanmd <mraihanf471@gmail.com>
2026-01-28 13:14:08 +0700

    bump

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
 #![allow(unexpected_cfgs)]
-#![cfg_attr(not(feature = "export-abi"), no_std)]
-#![cfg_attr(not(feature = "export-abi"), no_main)]
+#![cfg_attr(not(feature = "export-abi"), no_std, no_main)]
 
 extern crate alloc;
 
 use alloc::vec;
 use alloc::vec::Vec;
-use stylus_sdk::{alloy_primitives::*, alloy_sol_types::sol, prelude::*};
+use stylus_sdk::{alloy_primitives::*, alloy_sol_types::*, prelude::*, storage::*};
 
 sol_storage! {
     #[entrypoint]
     struct GainJar {
-        mapping(address => Employee) employees;
+        mapping(address => mapping(address => Employee)) employees;
+
+        mapping(address => address[]) employee_to_allowed_tokens;
+
+        mapping(address => mapping(address => uint256)) employer_token_balances;
     }
 
     struct Employee {
@@ -24,10 +27,54 @@ sol_storage! {
 }
 
 sol! {
+    error EmployeeAlreadyExists();
+    error EmployeeNotFound();
+    error InsufficientFunds();
+    error Unauthorized();
+    error InvalidPaymentInterval();
+    error TokenNotAllowed();
+    error TokenAlreadyAllowed();
+
     event EmployeeAdded(address indexed employer, address indexed employee);
     event PaymentExecuted(address indexed employer, address indexed employee, uint256 amount);
     event FundsDeposited(address indexed employer, uint256 amount);
 }
 
+#[derive(SolidityError)]
+pub enum GainJarErrors {
+    EmployeeAlreadyExists(EmployeeAlreadyExists),
+    EmployeeNotFound(EmployeeNotFound),
+    InsufficientFunds(InsufficientFunds),
+    Unauthorized(Unauthorized),
+    InvalidPaymentInterval(InvalidPaymentInterval),
+    TokenNotAllowed(TokenNotAllowed),
+    TokenAlreadyAllowed(TokenAlreadyAllowed),
+}
+
 #[public]
-impl GainJar {}
+impl GainJar {
+    pub fn add_allowed_token(&mut self, token: Address) -> Result<(), GainJarErrors> {
+        let mut allowed_tokens = self
+            .employee_to_allowed_tokens
+            .setter(self.vm().msg_sender());
+
+        for i in 0..allowed_tokens.len() {
+            if allowed_tokens.get(i).unwrap() == token {
+                return Err(GainJarErrors::TokenAlreadyAllowed(TokenAlreadyAllowed));
+            }
+        }
+
+        allowed_tokens.push(token);
+
+        Ok(())
+    }
+
+    pub fn add_employee(
+        &mut self,
+        employee: Address,
+        salary_amount: U256,
+        payment_interval: U256,
+    ) -> Result<(), GainJarErrors> {
+        Ok(())
+    }
+}
