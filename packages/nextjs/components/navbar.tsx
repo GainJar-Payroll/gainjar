72c11706d67f3884e76a06c861276da7c0d0b8ba
syafiqparadisam <syafiqparadisam@gmail.com>
2026-02-06 21:58:32 +0700

    feat(dashboard): adding menu in dashboard

--- packages/nextjs/components/navbar.tsx
+++ packages/nextjs/components/navbar.tsx
@@ -3,16 +3,16 @@ import { RainbowKitConnectButton } from "./rainbow-kit-connect-button";
 
 export default function Navbar() {
   return (
-    <nav className="p-4 flex justify-between max-w-7xl mx-auto items-center sticky top-0 backdrop-blur-sm bg-background/10 z-[1]">
+    <nav className="p-4 flex justify-between w-full mx-auto items-center sticky top-0 backdrop-blur-sm bg-background/10 z-[1]">
       <Link href="/" className="font-heading font-bold">
-        gainjar
+        Gainjar
       </Link>
-      <div className="flex justify-center gap-4 font-mono uppercase text-xs">
-        {/* <Link href={"/dashboard"} className="hover:underline">
+      <div className="flex justify-center items-center gap-4 font-mono uppercase text-xs">
+        <Link href={"/dashboard"} className="hover:underline">
           Dashboard
-        </Link> */}
-      </div>
+        </Link>
       <RainbowKitConnectButton />
+      </div>
     </nav>
   );
 }
