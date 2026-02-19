d0468282590646c0d19b6f20d67da27a54fce8e4
raihanmd <mraihanf471@gmail.com>
2026-02-19 11:46:17 +0700

    fix: form block fetch

--- packages/nextjs/hooks/useEmployersWithStreams.ts
+++ packages/nextjs/hooks/useEmployersWithStreams.ts
@@ -9,7 +9,7 @@ export function useEmployersWithStreams(employeeAddress: string | undefined) {
   const { data: streamCreatedEvents, isFetchingNewEvent: isLoading } = useScaffoldEventHistory({
     contractName: "GainJar",
     eventName: "StreamCreated",
-    fromBlock: 241942168n,
+    fromBlock: 243782709n,
     filters: { _employee: employeeAddress },
     watch: true,
   });
